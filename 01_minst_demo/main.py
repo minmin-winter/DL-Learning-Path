6a4433534b9b3bfcf4c5dbc32097e4c6994cc2b1
minmin-winter <weiboc83@gmail.com>
2025-12-14 10:06:46 +0800

    写完了MNIST数字识别模型了，hello，DL

--- 01_minst_demo/main.py
+++ 01_minst_demo/main.py
@@ -90,5 +90,107 @@ def imshow(img):
 
 #2.展示前4张图片
 
-print("标签：",' '.join(f"{labels[j].item()}" for j in range(4)))
-imshow(torchvision.utils.make_grid(images[:4]))
+#print("标签：",' '.join(f"{labels[j].item()}" for j in range(4)))
+#imshow(torchvision.utils.make_grid(images[:4]))
+
+
+# ------ 第二步：搭建神经网络模型 ------
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+# 1.定义网络结构
+class SimpleNet(nn.Module):
+    def __init__(self):
+        super(SimpleNet,self).__init__()
+        #第一层 ： 输入图片的像素点（28*28） -> 输出128（隐藏层节点数）
+        self.fc1 = nn.Linear(28*28,128)
+        #第二层 :  输入隐藏层节点数（128） -> 输出标签数(0-9 10个数)
+        self.fc2 = nn.Linear(128,10)
+
+    def forward(self,x):
+        # x 的形状一开始是[batch_size,1,28,28]
+        # view(-1,  )把图片变成[batch_size,28*28]的数据形式
+        x = x.view(-1,28*28)
+
+        # 通过第一层全连接，再通过ReLU非线性函数
+        x = F.relu(self.fc1(x))
+
+        # 再通过第二层输入层
+        x = self.fc2(x)
+        #注意： 不需要加Softmax，因为后面的Loss函数会自带
+        return x
+        
+# 2. 实例化模型
+model = SimpleNet()
+
+# 3. 定义损失函数和优化器
+criterion = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(),lr=0.01)
+
+#print(f"模型结构:{model}")
+
+
+#---------- 第三步：训练循环 ---------
+def train():
+
+    model.train()
+
+    #跑epochs遍整个数据集
+    epochs = 3
+
+    #
+    loss_history = []
+
+    for epoch in range(epochs):
+        for batch_idx, (data,target) in enumerate(train_loader):
+            # A. 梯度清零
+            optimizer.zero_grad()
+
+            # B. 前向传播
+            output = model(data)
+
+            # C. 算损失
+            loss = criterion(output,target)
+
+            # D. 后向传播
+            loss.backward()
+
+            # E. 更新参数
+            optimizer.step()
+
+            loss_history.append(loss.item())
+
+            # F. 打印一下进度
+            if not batch_idx% 100:
+                print(f"Epoch : {epoch+1} | Batch : {batch_idx} | Loss : {loss : .4f}")
+    
+    plt.plot(loss_history)
+    plt.title("Training Loss Curve")
+    plt.xlabel("Steps")
+    plt.ylabel("Loss")
+    plt.show() # 这会弹出一个窗口显示折线图
+
+def test():
+    model.eval()
+    test_loss = 0
+    correct = 0
+
+    with torch.no_grad():
+        for data,target in test_loader:
+             output = model(data)
+
+             test_loss += criterion(output,target).item()
+
+             pred = output.argmax(dim=1,keepdim=True)
+
+             correct += pred.eq(target.view_as(pred)).sum().item()
+
+        test_loss /= len(test_loader.dataset)
+        accuracy = correct / len(test_loader.dataset)
+
+        print(f"\n测试集结果: Loss:{test_loss:.4f} 准确率：{correct} /{len(test_loader.dataset)} (accuracy : {accuracy :.2%})\n")
+
+
+if __name__ == "__main__":
+    train()
+    test()
